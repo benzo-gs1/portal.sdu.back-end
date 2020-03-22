100053695977198fa1784a387e1999528d1c54e4
RayDarar <dfqgth400@gmail.com>
2020-03-22 23:02:27 +0600

    mongo::connected event created

--- src/loaders/db-loader.js
+++ src/loaders/db-loader.js
@@ -1,5 +1,11 @@
 import mongoose from "mongoose";
+import config from "@/config";
+import pipe from "@/pipe";
 
 export async function init() {
-  await mongoose.connect();
+  await mongoose.connect(config.mongodb.uri);
+
+  mongoose.connection.on("open", () => {
+    pipe.emit("mongo::connected");
+  });
 }
