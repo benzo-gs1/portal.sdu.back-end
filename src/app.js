8096deedfd13d5ea515306863352e4ffc6c1b310
Azamat Rsymbetov <seetwopluses@gmail.com>
2020-04-03 18:23:17 +0600

    Added logging using morgan

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import config from "./config";
 import { setConfig } from "./config";
 import Loaders from "./loaders";
+import morgan from 'morgan'
+import fs from 'fs'
+import path from 'path'
+
 
 /**
  * Entry point of the system
@@ -13,6 +17,10 @@ async function main(args) {
   const server = app.listen(config.port, () => {
     console.log(`Server runs at port ${config.port}`);
   });
+
+  const accessLogStream = fs.createWriteStream(path.join(__dirname, '../logs/access.log'), { flags: 'a' });
+
+  app.use(morgan(':date[clf] -> :method :url :status', { stream: accessLogStream }));
 
   setConfig("server", server);
 }
