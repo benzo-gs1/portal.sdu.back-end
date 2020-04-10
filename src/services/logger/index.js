5d14f4c32405f4ce99e61fdcf2a3399dfe32c9ad
RayDarar <dfqgth400@gmail.com>
2020-04-10 15:57:14 +0600

    logger methods defined | docs

--- src/services/logger/index.js
+++ src/services/logger/index.js
@@ -1,3 +1,36 @@
 import config from "@/config";
 
-export default class Logger {}
+export default class Logger {
+  static route(req) {
+    if (config.isProduction) {
+      // send task to job queue
+    } else {
+      // dev only
+      console.info(`[${this.getCurrentTime()}] -> ${req.method} request at ${req.path}`);
+    }
+  }
+
+  static fired(name) {
+    if (config.isProduction) {
+      // send task to job queue
+    } else {
+      // dev only
+      console.warn(`[${this.getCurrentTime()}] -> ${name} event fired`);
+    }
+  }
+
+  static handled(name, who, message) {
+    if (config.isProduction) {
+      // send task to job queue
+    } else {
+      // dev only
+      console.log(
+        `[${this.getCurrentTime()}] -> ${name} event handled by ${who}\nMessage: ${message}`
+      );
+    }
+  }
+
+  static getCurrentTime() {
+    return new Date().toLocaleString();
+  }
+}
