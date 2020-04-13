fc0ec76f47cb413c3d5077abe8ffca9ae6c45ffa
RayDarar <dfqgth400@gmail.com>
2020-04-13 18:27:48 +0600

    server::close logged

--- src/services/server/index.js
+++ src/services/server/index.js
@@ -1,5 +1,6 @@
 import pipe from "@/pipe";
 import config from "@/config";
+import Logger from "@/services/logger";
 
 class ServerService {
   static closeServer(server) {
@@ -8,6 +9,7 @@ class ServerService {
 }
 
 pipe.on("server::close", () => {
+  Logger.handled("server::close", "Server Service", "closing server...");
   ServerService.closeServer(config.server);
 });
 
