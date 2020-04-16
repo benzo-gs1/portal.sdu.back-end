0c0344cdeef4bef1cf8789fb734d1b84bc498348
RayDarar <dfqgth400@gmail.com>
2020-04-16 15:29:00 +0600

    pipe/names refactored

--- src/pipe/names.js
+++ src/pipe/names.js
@@ -1,14 +1,4 @@
-function deepFreeze(object) {
-  const isObject = obj => obj === Object(obj);
-
-  let propNames = Object.getOwnPropertyNames(object);
-  for (let name of propNames) {
-    let value = object[name];
-    object[name] = value && isObject(value) ? deepFreeze(value) : value;
-  }
-
-  return Object.freeze(object);
-}
+import { deepFreeze } from "@/utils";
 
 export default deepFreeze({
   server: {
