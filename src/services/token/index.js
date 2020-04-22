847760cace61d0c24247e98fbf40825e42221981
RayDarar <dfqgth400@gmail.com>
2020-04-22 22:35:58 +0600

    token-service comments added

--- src/services/token/index.js
+++ src/services/token/index.js
@@ -1,25 +1,46 @@
 import jwt from "jsonwebtoken";
 import config from "@/config";
+import Logger from "@/services/logger";
 
 class TokenService {
+  /**
+   * @description Creating user access token. Ip address, role level and username must be passed as data
+   *
+   * @returns signed jwt token or false in case of error
+   */
   static create(data, lifeTime = "1h") {
-    if (data.ip && data.role_level && data.username) {
-      return jwt.sign(data, config.secretKey, {
-        expiresIn: lifeTime,
-        algorithm: config.secretAlgorithm,
-      });
-    }
-    return false;
-  }
-
-  static validate(token) {
     try {
-      return jwt.verify(token, config.secretKey);
+      if (data.ip && data.role_level && data.username) {
+        return jwt.sign(data, config.secretKey, {
+          expiresIn: lifeTime,
+          algorithm: config.secretAlgorithm,
+        });
+      }
     } catch (err) {
+      Logger.error("Token Service", err);
       return false;
     }
   }
 
+  /**
+   * @description validates given token
+   *
+   * @returns encapsulated data or false in case token is invalid
+   */
+  static validate(token) {
+    try {
+      return jwt.verify(token, config.secretKey);
+    } catch (err) {
+      Logger.error("Token Service", err);
+      return false;
+    }
+  }
+
+  /**
+   * @description middleware function for token validation
+   *
+   * @returns sets req.token property with token's payload or sends 403 HTTP status code
+   */
   static middle(req, res, next) {
     const header = req.headers["authorization"];
 
