f8664be2cbdbaed9da1ab9e2f8cf5597bf665802
RayDarar <dfqgth400@gmail.com>
2020-05-02 18:46:37 +0600

    configuring db connection

--- src/loaders/db-loader.js
+++ src/loaders/db-loader.js
@@ -8,9 +8,14 @@ export async function init() {
   if (!config.isTesting)
     mongoose.connection.on("open", () => pipe.emit(events.mongo.connected));
 
+  // use poolSize of 10 in production and 1 in development
+  const poolSize = config.isProduction ? 10 : 1;
+
   return await mongoose.connect(config.mongodbUri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
+    autoIndex: false,
+    poolSize,
   });
 }
--- test/db.js
+++ test/db.js
@@ -1,2 +1,9 @@
 import { expect } from "chai";
 import { init } from "@/loaders/db-loader";
+
+let connection;
+
+describe("Database", function () {
+  // opening connection
+  this.beforeEach(async function () {});
+});
