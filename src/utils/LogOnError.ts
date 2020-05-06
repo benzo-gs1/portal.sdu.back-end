80f7915b39ca2e14527a36a2593c0a565c1434e7
RayDarar <dfqgth400@gmail.com>
2020-05-06 12:36:10 +0600

    log-on-error decorator utility created

---
+++ src/utils/LogOnError.ts
@@ -0,0 +1,19 @@
+import Logger from "@/services/logger";
+
+export default function LogOnError(
+  target: Function,
+  key: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value as Function;
+
+  descriptor.value = async function (...args: any[]) {
+    try {
+      return await originalMethod.apply(this, args);
+    } catch (err) {
+      Logger.log(err);
+    }
+  };
+
+  return descriptor;
+}
