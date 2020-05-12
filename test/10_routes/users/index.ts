d2a75cc0d37c0cb76e1e26aa3951eb29c5a230e8
RayDarar <dfqgth400@gmail.com>
2020-05-12 20:22:31 +0600

    api/users/validate tests written

--- test/10_routes/users/index.ts
+++ test/10_routes/users/index.ts
@@ -1,5 +1,6 @@
 import testCreate from "./test/create";
 import testDelete from "./test/delete";
 import authorize from "./authorize";
+import validate from "./validate";
 
-export default [testCreate, authorize, testDelete];
+export default [testCreate, validate, authorize, testDelete];
